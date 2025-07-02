21b458b7bccf8d4f6477972764ef77b86600a1ad
Jafar-Moore <162931116+Jafar-Moore@users.noreply.github.com>
2025-07-01 22:09:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 
 app.use(express.urlencoded({ extended: true }));
